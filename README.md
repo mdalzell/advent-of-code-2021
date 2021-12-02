3af47f01a08006d3e42811a69d587d409e847184
Matt Dalzell <contact@mattdalzell.com>
2021-12-01 20:47:04 -0600

    Day 1

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repository contains my solutions to the 2021 Advent of Code, implemented us
 
 ## Run Run Rudolph
 
-To compute the solution for a given day's problems, you can run the npm scripts found in `package.json`. For example, to see the solutions for day one's problems:
+To compute the solution for a given day's problems, you can run the npm scripts found in `package.json` from the root directory of the project. For example, to see the solutions for day one's problems:
 
 ```bash
 $ npm run one
--- src/solutions/one.ts
+++ src/solutions/one.ts
@@ -1,1 +1,40 @@
-console.log("Day 1 Placeholder");
+import { readFileSync } from "fs";
+
+const readInput = (filePath: string) => {
+    const data = readFileSync(filePath, 'utf-8');
+    return data.split(/\r?\n/)
+}
+
+const dayOnePartOne = () => {
+    const depths = readInput("input/day-one.txt");
+
+    let increaseCount = 0;
+    depths.reduce((previous, current) => {
+        if (+current > +previous) increaseCount++;
+        return current;
+    })
+
+    console.log("Day One - Part One:", increaseCount)
+}
+
+const dayOnePartTwo = () => {
+    const depths = readInput("input/day-one.txt");
+
+    let increaseCount = 0;
+    const depthWindow: number[] = [];
+    depths.forEach((depth) => {
+        const current = depthWindow.reduce((a, b) => a + b, 0);
+        depthWindow.push(+depth)
+        if (depthWindow.length === 4){
+            depthWindow.shift();
+            const next = depthWindow.reduce((a, b) => a + b, 0)
+            if (next > current) increaseCount++;
+        }
+    })
+
+    console.log("Day One - Part Two:", increaseCount)
+}
+
+
+dayOnePartOne()
+dayOnePartTwo()
