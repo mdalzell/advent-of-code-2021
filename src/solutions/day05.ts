579b8da4e380be2d78b7a2f188e21adc9228b0d8
Matt Dalzell <contact@mattdalzell.com>
2021-12-05 15:30:01 -0600

    Day 5 Refactors

--- src/solutions/day05.ts
+++ src/solutions/day05.ts
@@ -4,56 +4,20 @@ type PointIntersectionCountMap = {
   [point: string]: number;
 };
 
-const buildPointIntersectionCount = (input: string[], includeDiagonal: boolean) => {
-  const pointIntersectionCount: PointIntersectionCountMap = {};
-
-  for (const line of input) {
-    const [startPoint, endPoint] = line.split(' -> ');
-    const [startPointX, startPointY] = startPoint.split(',').map((num) => parseInt(num));
-    const [endPointX, endPointY] = endPoint.split(',').map((num) => parseInt(num));
-
-    if (startPointX === endPointX) {
-      for (let i = Math.min(startPointY, endPointY); i <= Math.max(startPointY, endPointY); i++) {
-        const key = `(${startPointX},${i})`;
-        if (pointIntersectionCount[key]) {
-          pointIntersectionCount[key]++;
-        } else {
-          pointIntersectionCount[key] = 1;
-        }
-      }
-    } else if (startPointY === endPointY) {
-      for (let i = Math.min(startPointX, endPointX); i <= Math.max(startPointX, endPointX); i++) {
-        const key = `(${i},${startPointY})`;
-        if (pointIntersectionCount[key]) {
-          pointIntersectionCount[key]++;
-        } else {
-          pointIntersectionCount[key] = 1;
-        }
-      }
-    } else if (includeDiagonal) {
-      const slope =
-        Math.min(startPointX, endPointX) === startPointX
-          ? (endPointY - startPointY) / (endPointX - startPointX)
-          : (startPointY - endPointY) / (startPointX - endPointX);
-      const b = startPointY - slope * startPointX;
-      for (let i = Math.min(startPointX, endPointX); i <= Math.max(startPointX, endPointX); i++) {
-        const key = `(${i},${slope * i + b})`;
-        if (pointIntersectionCount[key]) {
-          pointIntersectionCount[key]++;
-        } else {
-          pointIntersectionCount[key] = 1;
-        }
-      }
-    }
-  }
-
-  return pointIntersectionCount;
+type Point = {
+  x: number;
+  y: number;
 };
 
-const day05 = (includeDiagonal: boolean) => {
-  const data = readInput('input/day05.txt');
-  const pointIntersectionCount = buildPointIntersectionCount(data, includeDiagonal);
+const addToPointIntersectionCount = (pointIntersectionCount: PointIntersectionCountMap, key: string) => {
+  if (pointIntersectionCount[key]) {
+    pointIntersectionCount[key]++;
+  } else {
+    pointIntersectionCount[key] = 1;
+  }
+};
 
+const countIntersections = (pointIntersectionCount: PointIntersectionCountMap) => {
   let intersectionCount = 0;
   for (const key in pointIntersectionCount) {
     if (pointIntersectionCount[key] > 1) {
@@ -64,5 +28,43 @@ const day05 = (includeDiagonal: boolean) => {
   return intersectionCount;
 };
 
-console.log('Day 5 - Part 1', day05(false));
-console.log('Day 5 - Part 2', day05(true));
+const buildPoint = (coordinates: string) => {
+  const [xValue, yValue] = coordinates.split(',');
+  return { x: parseInt(xValue), y: parseInt(yValue) };
+};
+
+const day05 = (diagonalFn: (pointIntersectionCount: PointIntersectionCountMap, start: Point, end: Point) => void = () => {}) => {
+  const data = readInput('input/day05.txt');
+  const pointIntersectionCount: PointIntersectionCountMap = {};
+
+  for (const line of data) {
+    const [startCoordinates, endCoordinates] = line.split(' -> ');
+    const start = buildPoint(startCoordinates);
+    const end = buildPoint(endCoordinates);
+
+    if (start.x === end.x) {
+      for (let i = Math.min(start.y, end.y); i <= Math.max(start.y, end.y); i++) {
+        addToPointIntersectionCount(pointIntersectionCount, `(${start.x},${i})`);
+      }
+    } else if (start.y === end.y) {
+      for (let i = Math.min(start.x, end.x); i <= Math.max(start.x, end.x); i++) {
+        addToPointIntersectionCount(pointIntersectionCount, `(${i},${start.y})`);
+      }
+    } else {
+      diagonalFn(pointIntersectionCount, start, end);
+    }
+  }
+
+  return countIntersections(pointIntersectionCount);
+};
+
+const calculateDiagonalLine = (pointIntersectionCount: PointIntersectionCountMap, start: Point, end: Point) => {
+  const slope = Math.min(start.x, end.x) === start.x ? (end.y - start.y) / (end.x - start.x) : (start.y - end.y) / (start.x - end.x);
+  const b = start.y - slope * start.x;
+  for (let i = Math.min(start.x, end.x); i <= Math.max(start.x, end.x); i++) {
+    addToPointIntersectionCount(pointIntersectionCount, `(${i},${slope * i + b})`);
+  }
+};
+
+console.log('Day 5 - Part 1', day05());
+console.log('Day 5 - Part 2', day05(calculateDiagonalLine));
