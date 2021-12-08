844e68174100891bed2ae952f971466678baeefc
Matt Dalzell <contact@mattdalzell.com>
2021-12-07 18:46:51 -0600

    Day 7 - Part 2

--- src/solutions/day07.ts
+++ src/solutions/day07.ts
@@ -1,22 +1,33 @@
 import { readInput } from '../shared/io';
 
-const calculateTotalFuel = (position: number, locations: number[]) => {
+const calculateTotalFuel = (position: number, locations: number[], fuelFn: FuelFn) => {
   let totalFuel = 0;
   for (const location of locations) {
-    totalFuel += Math.abs(location - position);
+    totalFuel += fuelFn(position, location);
   }
 
   return totalFuel;
 };
 
-const day07 = () => {
+type FuelFn = (position1: number, position2: number) => number;
+
+const part1Fuel = (position1: number, position2: number) => {
+  return Math.abs(position1 - position2);
+};
+
+const part2Fuel = (position1: number, position2: number) => {
+  const absoluteDistance = Math.abs(position1 - position2);
+  return (absoluteDistance ** 2 + absoluteDistance) / 2;
+};
+
+const day07 = (fuelFn: FuelFn) => {
   const data = readInput('input/day07.txt');
   const locations = data[0].split(',').map((num) => parseInt(num));
   const max = Math.max(...locations);
 
   let minimumFuel = Number.MAX_VALUE;
   for (let i = 0; i < max; i++) {
-    const fuelUsed = calculateTotalFuel(i, locations);
+    const fuelUsed = calculateTotalFuel(i, locations, fuelFn);
     if (fuelUsed > minimumFuel) {
       return minimumFuel;
     }
@@ -27,4 +38,5 @@ const day07 = () => {
   return minimumFuel;
 };
 
-console.log('Day 7 - Part 1', day07());
+console.log('Day 7 - Part 1', day07(part1Fuel));
+console.log('Day 7 - Part 2', day07(part2Fuel));
