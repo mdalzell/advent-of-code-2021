1bee66e40f8a05dda84f99b1629bb8c0fc5cf514
Matt Dalzell <contact@mattdalzell.com>
2021-12-10 15:04:31 -0600

    Day 10 - Part 2

--- src/solutions/day10.ts
+++ src/solutions/day10.ts
@@ -7,14 +7,35 @@ const illegalCharacterValues: { [key: string]: number } = Object.freeze({
   '>': 25137,
 });
 
-const openingCharacterMap: { [key: string]: string } = Object.freeze({
-  ')': '(',
-  ']': '[',
-  '}': '{',
-  '>': '<',
+const autocompleteCharacterValues: { [key: string]: number } = Object.freeze({
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
 });
 
-const bracketChecker = (line: string) => {
+const closingCharacterMap: { [key: string]: string } = Object.freeze({
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
+});
+
+const autoCompleteScore = (bracketStack: string[]) => {
+  let closingSequence = '';
+  while (bracketStack.length !== 0) {
+    closingSequence += closingCharacterMap[bracketStack.pop()!];
+  }
+
+  return closingSequence.split('').reduce((acc, char) => {
+    return acc * 5 + autocompleteCharacterValues[char];
+  }, 0);
+};
+
+type AutoCompleteFn = (bracketStack: string[]) => number;
+type ErrorFn = (char: string) => number;
+
+const bracketChecker = (line: string, autocompleteFn: AutoCompleteFn, errorFn: (char: string) => number) => {
   const bracketStack = [];
 
   for (const char of line) {
@@ -22,21 +43,44 @@ const bracketChecker = (line: string) => {
       bracketStack.push(char);
     } else if ([')', ']', '}', '>'].includes(char)) {
       const lastBracket = bracketStack.pop();
-      if (lastBracket !== openingCharacterMap[char]) {
-        return illegalCharacterValues[char];
+      if (closingCharacterMap[lastBracket!] !== char) {
+        return errorFn(char);
       }
     }
   }
 
-  return 0;
+  return autocompleteFn(bracketStack);
 };
 
-const day10 = () => {
-  const data = readInput('input/day10.txt');
+type SyntaxScoreFn = (lines: string[]) => number;
 
-  return data.reduce((acc, line) => {
-    return acc + bracketChecker(line);
+const part1: SyntaxScoreFn = (lines: string[]) => {
+  return lines.reduce((acc, line) => {
+    return (
+      acc +
+      bracketChecker(
+        line,
+        () => 0,
+        (char) => illegalCharacterValues[char],
+      )
+    );
   }, 0);
 };
 
-console.log('Day 10 - Part 1', day10());
+const part2: SyntaxScoreFn = (lines: string[]) => {
+  const autoCompleteScores = lines
+    .map((line) => bracketChecker(line, autoCompleteScore, () => 0))
+    .filter((score) => score !== 0)
+    .sort((a, b) => a - b);
+
+  return autoCompleteScores[Math.round((autoCompleteScores.length - 1) / 2)];
+};
+
+const day10 = (syntaxScoreFn: SyntaxScoreFn) => {
+  const data = readInput('input/day10.txt');
+
+  return syntaxScoreFn(data);
+};
+
+console.log('Day 10 - Part 1', day10(part1));
+console.log('Day 10 - Part 2', day10(part2));
